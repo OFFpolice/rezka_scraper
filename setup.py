1189ac228ac369cad5f1b15d9b8aac95bd9fcfe6
OFFpolice <offpolice2077@gmail.com>
2024-11-13 11:17:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@
 import pathlib
 from setuptools import setup, find_packages
 
-# Путь к README.md
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='rezka_scraper',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'aiohttp',
         'beautifulsoup4',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     description='RezkaScraper — это библиотека на Python для асинхронного поиска контента (аниме, фильмов, сериалов и мультфильмов) на сайте Rezka.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -24,7 +23,6 @@ setup(
     url='https://github.com/OFFpolice/rezka_scraper',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
