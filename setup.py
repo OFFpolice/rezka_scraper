7d1bb2115b5eb4fd51052b59b72d866363107e82
OFFpolice <offpolice2077@gmail.com>
2025-01-09 13:20:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@
 import pathlib
 from setuptools import setup, find_packages
 
+
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
+
 
 setup(
     name='rezka_scraper',
